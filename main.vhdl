ae8a043323f35cfe4f35eb14c946bc2abb4a8d0f
Matthias <mail@matthiasbock.net>
2015-08-16 20:32:41 +0200

    receive a byte via UART

--- main.vhdl
+++ main.vhdl
@@ -2,10 +2,53 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 
-entity JTAG_sniffer is
+entity JTAGsniffer is
     port(
-        clock_12MHz: in std_logic;
+        --
+        -- Onboard quartz
+        -- Frequency: 12MHz
+        --
+    
+        clock: in std_logic;
         -- J3: PIO3_26
+        
+        --
+        -- Onboard red LEDs
+        -- Required voltage: 3.3V ?
+        --
+
+        LED0 : out std_logic;
+        -- B5: PIO0_39
+        LED1 : out std_logic;
+        -- B4: PIO0_41
+        LED2 : out std_logic;
+        -- A2: PIO0_42
+        LED3 : out std_logic;
+        -- A1: PIO0_44
+        LED4 : out std_logic;
+        -- C5: PIO0_45
+        LED5 : out std_logic;
+        -- C4: PIO0_46
+        LED6 : out std_logic;
+        -- B3: PIO0_47
+        LED7 : out std_logic;
+        -- C3: PIO0_51
+
+        --
+        -- RS232 via FTDI-2232
+        -- Voltage level: 3.3V
+        -- Baud: 115200bps 8N1
+        --
+        
+        RX : in std_logic;
+        -- B10: PIO0_14
+        TX : out std_logic;
+        -- B12: PIO0_13
+
+        --
+        -- JTAG tap
+        -- Input signal voltage: 1.2V
+        --
         
         TCLK : in std_logic;
         TRST : in std_logic;
@@ -15,9 +58,21 @@ entity JTAG_sniffer is
     );
 end entity;
 
-architecture main of JTAG_sniffer is
+architecture main of JTAGsniffer is
 
+    --
+    -- RS232 signals
+    --
+    signal UART_clock : std_logic := '0';
+    type UART_FSM is (idle, receiving, stop);
+    signal UART_state : UART_FSM := idle;
+    signal UART_received_byte : std_logic_vector(7 downto 0) := (others => '0');
+    signal UART_byte_ready : std_logic := '0';
+    
+
+    --
     -- JTAG state machine
+    --
     type JTAG_FSM is (
         test_logic_reset,
         run_test_idle,
@@ -42,7 +97,83 @@ architecture main of JTAG_sniffer is
 
 begin
 
-    -- state machine follower
+    --
+    -- Clock divider for UART
+    --
+    process(clock, RX)
+        variable counter : integer range 0 to 104167 := 0;
+    begin
+        if clock'event and clock='1' then
+            if counter = 104167 then
+                UART_clock <= '1';
+                counter := 0; 
+            else
+                if counter = 52083 then
+                    UART_clock <= '0';
+                end if;
+                counter := counter + 1;
+            end if;
+        end if;
+    end process;
+
+    --
+    -- UART receiver
+    --
+    process(UART_clock, RX)
+        variable UART_received_bits : integer range 0 to 7 := 0;
+    begin
+        if UART_clock'event and UART_clock='1' then
+
+            case UART_state is
+            
+                -- one start bit
+                when idle =>
+                    UART_byte_ready <= '0';
+                    if RX='1' then
+                        UART_state <= receiving;
+                        UART_received_bits := 0;
+                        UART_received_byte <= (others => '0');
+                    end if;
+                
+                -- 8 data bits
+                when receiving =>
+                    -- shift right and concatenate
+                    UART_received_byte <= RX & UART_received_byte(7 downto 1); 
+                    UART_received_bits := UART_received_bits + 1;  
+                    if UART_received_bits >= 8 then
+                        UART_state <= stop;
+                        UART_byte_ready <= '1';
+                    end if;
+                    
+                -- one stop bit
+                when stop =>
+                    UART_state <= idle;
+
+            end case;
+
+        end if;
+    end process;
+
+    --
+    -- Output received UART byte to LEDs
+    --
+    process(UART_byte_ready)
+    begin
+        if UART_byte_ready'event and UART_byte_ready='1' then
+            LED0 <= UART_received_byte(0);
+            LED1 <= UART_received_byte(1);
+            LED2 <= UART_received_byte(2);
+            LED3 <= UART_received_byte(3);
+            LED4 <= UART_received_byte(4);
+            LED5 <= UART_received_byte(5);
+            LED6 <= UART_received_byte(6);
+            LED7 <= UART_received_byte(7);
+        end if;
+    end process;
+
+    --
+    -- JTAG state machine follower
+    --
     process(TRST,TCLK,TMS)
     begin
         -- flip-flop
